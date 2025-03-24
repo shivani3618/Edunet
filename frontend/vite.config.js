188e39342e1d211067bd2dde49733a02c5d6a6a3
shivani <shivanipuppala.3618@gmail.com>
2025-03-24 21:28:28 +0530

    first commit

---
+++ frontend/vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig(({ mode }) => {
+  // Load environment variables
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return {
+    plugins: [react()],
+  };
+});
